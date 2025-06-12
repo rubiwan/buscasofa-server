7a06c6d7b090846facee732f35f942a2910c9177
rubiwan <nadakepensar@gmail.com>
2025-06-12 15:53:52 +0200

    TEST-RED(userController.js) reescribimos test aumentando la funcionalidad

--- tests/userController.test.js
+++ tests/userController.test.js
@@ -1,5 +1,8 @@
 const { registerUser } = require('../controllers/userController');
 
+/**
+ * Prueba unitaria para el registro de usuarios
+ */
 describe('registerUser', () => {
     it('debería rechazar si falta el email', () => {
         const req = {
@@ -15,5 +18,32 @@ describe('registerUser', () => {
 
         expect(res.status).toHaveBeenCalledWith(400);
         expect(res.json).toHaveBeenCalledWith({ message: 'Todos los campos son obligatorios' });
+    });
+});
+
+/**
+ * Control de usuarios repetidos
+ */
+it('debería rechazar si el usuario ya existe', (done) => {
+    const req = {
+        body: { username: 'ana', email: 'ana@mail.com', password: '1234' }
+    };
+    const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+    };
+
+    const db = {
+        get: (sql, params, callback) => {
+            callback(null, { id: 1 }); // simula usuario
+        }
+    };
+
+    registerUser(req, res, db);
+
+    setImmediate(() => {
+        expect(res.status).toHaveBeenCalledWith(409);
+        expect(res.json).toHaveBeenCalledWith({ message: 'Usuario o email ya existe' });
+        done();
     });
 });
